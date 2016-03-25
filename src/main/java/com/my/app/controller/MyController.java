942582a6340b5b78640958b80cd0e1f07e3e3c1f
lininxiao <linjinxiao@gmail.conm>
2016-03-25 17:12:35 +0800

    增加session管理功能

--- src/main/java/com/my/app/controller/MyController.java
+++ src/main/java/com/my/app/controller/MyController.java
@@ -1,5 +1,9 @@
 package com.my.app.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,9 +17,30 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MyController {
 
+	@Autowired
+	private HttpServletRequest request;
+
 	@RequestMapping("login") //用来处理前台的login请求  
 	private @ResponseBody String hello(@RequestParam(value = "username", required = false) String username,
 			@RequestParam(value = "password", required = false) String password) {
+
+		HttpSession jes = request.getSession(false);
+
+		if (jes == null) {
+			if ("a".equals(username)) {
+				System.out.println("begin create");
+				jes = request.getSession(true);
+				jes.setAttribute("aa", "bbc");
+			} else {
+				System.out.println("not create");
+			}
+		} else {
+			Object aa = jes.getAttribute("aa");
+			System.out.println("aa:" + aa);
+		}
+
+		//		System.out.println("JSESSIONID:" + jes.toString());
+
 		return "Hello " + username + ",Your password is: " + password;
 
 	}
